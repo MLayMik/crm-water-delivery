3ac6c6768fa05ef497be5536b1c4ddcf0bb93068
mlaymik <maximmostov2@gmail.com>
2025-07-09 15:32:19 +0200

    feat(input)

---
+++ src/shared/ui/CInput/WInput.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+import { WInput } from '.'
+
+export default {
+  title: 'Shared/WInput',
+  component: WInput,
+  tags: ['autodocs'],
+
+  args: {
+    label: '',
+    small: false,
+  },
+
+  argTypes: {
+    class: { table: { disable: true } },
+  },
+} satisfies Meta<typeof WInput>
+
+type Story = StoryObj<typeof WInput>
+
+export const Default: Story = {}
+
+export const WithLabel: Story = { args: { label: 'Label' } }
+
+export const Small: Story = { args: { small: true } }
---
+++ src/shared/ui/CInput/WInput.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { ClassValue } from 'clsx'
+import type { InputHTMLAttributes } from 'vue'
+
+import { cn } from '@/shared/lib/styles'
+
+export interface Props extends /* @vue-ignore */ InputHTMLAttributes {
+  class?: ClassValue
+  label?: string
+  small?: boolean
+}
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const props = defineProps<Props>()
+
+const modelValue = defineModel<string>()
+</script>
+
+<template>
+  <div class="w-full">
+    <label
+      v-if="props.label"
+      class="
+        mb-1 inline-block text-sm font-medium text-slate-700 transition-colors
+      "
+    >
+      {{ props.label }}
+    </label>
+
+    <input
+      v-model="modelValue"
+      v-bind="$attrs"
+      :class="cn(
+        `
+          w-full rounded-xl border border-slate-300 bg-white px-4 py-2 text-base
+          text-slate-800 shadow-sm transition-all duration-200 ease-in-out
+          placeholder:text-slate-400 placeholder:transition-opacity
+          placeholder:duration-200
+          hover:border-slate-400
+          focus:border-blue-500 focus:ring-2 focus:ring-blue-400/30
+          focus:outline-none
+          disabled:cursor-not-allowed disabled:opacity-50
+        `,
+        props.small && 'px-3 py-1.5 text-sm',
+        props.class,
+      )"
+    >
+  </div>
+</template>
