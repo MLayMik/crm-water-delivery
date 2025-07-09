a852dd586400dee45d5586986d7c54e30a57a79e
mlaymik <maximmostov2@gmail.com>
2025-07-09 15:31:37 +0200

    style(auth): wip styling

--- src/features/auth/ui/LoginForm.vue
+++ src/features/auth/ui/LoginForm.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
+import { Eye } from 'lucide-vue-next'
+import WInput from '@/shared/ui/CInput/WInput.vue'
 </script>
 
 <template>
-    <form action="">
-        <div>
-            <p>
-                asdflkasjgdf
-            </p>
+  <form action="">
+    <div class="font-poppins px-10 py-20">
+      <p class="mb-8 text-3xl">
+        Sign in asdf
+      </p>
+      <div class="mb-8">
+        <p class="mb-2 text-[#666666]">
+          Email or phone number
+        </p>
+        <WInput />
+      </div>
+      <div>
+        <div class="mb-2 flex justify-between text-[#666666]">
+          <p>Password</p>
+          <p class="flex gap-2">
+            <Eye />
+            Hide
+          </p>
         </div>
-    </form>
+        <WInput />
+      </div>
+    </div>
+  </form>
 </template>
+/* Rectangle 5 */
+
+position: absolute;
+width: 1440px;
+height: 1024px;
+left: 0px;
+top: 0px;
+
+background: rgba(0, 0, 0, 0.5);
+backdrop-filter: blur(4px);
+/* Note: backdrop-filter has minimal browser support */
---
+++ src/pages/layouts/AuthLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <RouterLink :to="{ name: 'Home' }">
+      <img
+        class="absolute top-4"
+        src=""
+        alt="logo"
+      >
+    </RouterLink>
+    <slot />
+  </div>
+</template>
