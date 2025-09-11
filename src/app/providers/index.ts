51a12ac6fce8390dc07e702fac0f2fad30a0e11b
mlaymik <maximmostov2@gmail.com>
2025-09-11 18:11:46 +0200

    feat: msw impl

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,15 +1,27 @@
 import type { App } from 'vue'
-import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persistedstate'
 
+import { ENVIRONMENT } from '@/shared/config'
 import { router } from './router'
 
 const pinia = createPinia()
 pinia.use(piniaPersist)
 
+async function enableMocking() {
+  if (ENVIRONMENT !== 'development') {
+    return
+  }
+
+  const { worker } = await import('@/shared/lib/utils/worker')
+
+  return worker.start()
+}
+
 export function applyProviders(app: App) {
   app.use(VueQueryPlugin)
   app.use(pinia)
   app.use(router)
+  app.use(enableMocking)
 }
