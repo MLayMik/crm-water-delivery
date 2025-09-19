28939c9e0b3858730eb4aba78a73321c2710be55
mlaymik <maximmostov2@gmail.com>
2025-09-19 14:52:59 +0200

    fix(auth): autofill

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -2,3 +2,5 @@ export const API_URL = import.meta.env.VITE_API_URL
 export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
 export const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
+export const SUPABASE_EMAIL = import.meta.env.VITE_SUPABASE_EMAIL
+export const SUPABASE_PASSWORD = import.meta.env.VITE_SUPABASE_PASSWORD
